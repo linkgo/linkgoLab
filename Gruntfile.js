e8f7d89e298a61633fb4e3948ce38c378ac74aa3
roybein <roybein@gmail.com>
2016-05-19 14:14:03 +0800

    home page ready
    1.import grunt to do less issue and watch
    2.import i18n to internationalize

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            main: {
+                src: 'public/javascripts/linkgo.js',
+                dest: 'public/javascripts/linkgo.min.js'
+            }
+        },
+        less: {
+            expanded: {
+                options: {
+                    paths: ["css"]
+                },
+                files: {
+                    "public/stylesheets/linkgo.css": "public/stylesheets/linkgo.less"
+                }
+            },
+            minified: {
+                options: {
+                    paths: ["css"],
+                    cleancss: true
+                },
+                files: {
+                    "public/stylesheets/linkgo.min.css": "public/stylesheets/linkgo.less"
+                }
+            }
+        },
+        banner: '/*!\n' +
+            ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+            ' * Copyright 2013-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' */\n',
+        usebanner: {
+            dist: {
+                options: {
+                    position: 'top',
+                    banner: '<%= banner %>'
+                },
+                files: {
+                    src: ['public/stylesheets/linkgo.css', 'public/stylesheets/linkgo.min.css', 'public/javascripts/linkgo.min.js']
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['public/javascripts/linkgo.js'],
+                tasks: ['uglify'],
+                options: {
+                    spawn: false,
+                },
+            },
+            less: {
+                files: ['public/stylesheets/*.less'],
+                tasks: ['less'],
+                options: {
+                    spawn: false,
+                }
+            },
+        },
+    });
+
+    // Load the plugins.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-banner');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify', 'less', 'usebanner', 'watch']);
+
+};
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var i18n = require('i18n');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -8,6 +9,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+
 var app = express();
 
 // view engine setup
@@ -15,11 +17,29 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// i18n.expressBind(app, {
+//     locales: ['en', 'zh'],
+//     cookieName: 'locale'
+// });
+// app.use(function(req, res, next) {
+//     req.i18n.setLocaleFromCookie();
+//     next();
+// });
+
+i18n.configure({
+  locales: ['en', 'zh'],
+  directory: __dirname + '/locales',
+  objectNotation: true,
+  queryParameter: 'lang'
+});
+app.use(i18n.init);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -56,5 +76,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
+console.log(i18n.__("brand"));
+console.log(i18n.__("home.slogan"));
 
 module.exports = app;
